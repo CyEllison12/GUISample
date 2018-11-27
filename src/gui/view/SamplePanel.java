c6bbf1f580eb76dd8373cdbd91efe46325e38d40
Cy Ellison <dragonz2626@gmail.com>
2018-11-27 13:46:20 -0700

    added the button and label, made button change background color

--- src/gui/view/SamplePanel.java
+++ src/gui/view/SamplePanel.java
@@ -2,10 +2,15 @@ package gui.view;
 
 import javax.swing.*;
 import gui.controller.GUIController;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class SamplePanel extends JPanel
 {
 	private GUIController appController;
+	private JLabel textLabel;
+	private JButton colorButton;
 	
 	public SamplePanel(GUIController appController)
 	{
@@ -13,6 +18,45 @@ public class SamplePanel extends JPanel
 		
 		this.appController = appController;
 		
+		this.textLabel = new JLabel("THIS IS A COLOR APP!");
+		this.colorButton = new JButton("CLICK HERE TO CHANGE COLOR");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		this.setBackground(Color.CYAN);
+		this.add(textLabel);
+		this.add(colorButton);
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		colorButton.addActionListener(new ActionListener()
+				{
+					public void actionPerformed(ActionEvent mouseClick)
+					{
+						changeBackgroundColor();
+					}
+				});
+	}
+	
+	private void changeBackgroundColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		
+		this.setBackground(new Color(red, green, blue));
+		textLabel.setText("Red: " + red + " Green: " + green + " Blue: " + blue);
 	}
 
 }
